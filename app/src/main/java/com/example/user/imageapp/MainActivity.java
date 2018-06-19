0707995d6ec51d713bd8113f5d2552615990c159
mazgana <adi.mazgani@gmail.com>
2018-06-19 18:13:34 +0300

    Adding progress bar and transform pics method

--- app/src/main/java/com/example/user/imageapp/MainActivity.java
+++ app/src/main/java/com/example/user/imageapp/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.user.imageapp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
